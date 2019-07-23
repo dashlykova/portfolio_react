08b9cb44c5e4ba21011acd44860093440d07a8c1
Daria Lykova <daria.lykova@gmail.com>
2019-07-23 14:40:12 +0200

    Creates a Hello & Header jsx file/Styles using css/Modifies index.js file by connecting it to jsx files

---
+++ src/Hello.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+
+const Hello = () => {
+    return (
+        <div className="page-container" style={container}>
+            <div style={name}>
+                <p>Daria Lykova</p>
+
+            </div>
+        </div>
+    )
+}
+
+const container = {
+    position: 'absolute',
+    width: '2000px',
+    height: '800px',
+    background: '#E83F3F',
+    margin: '0',
+    padding: '0',
+    border: 'none'
+}
+
+const name = {
+    position: 'center',
+    width: '268px',
+    height: '100px',
+    paddingLeft:'25rem',
+    paddingTop: '30rem',
+   
+
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '90px',
+    lineHeight: '64px',
+    /* or 58px */
+    letterSpacing: '-0.12em',
+    color: '#E5E5E5'
+}
+
+
+
+
+export default Hello
