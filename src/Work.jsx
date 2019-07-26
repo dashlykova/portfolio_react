2461ebfa445ade8120464d6904291b6655609c24
Daria Lykova <daria.lykova@gmail.com>
2019-07-26 13:46:56 +0200

    Adds info in works.json/Connects WorkCard to Work

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,13 +1,47 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
+import WorkCard from "./WorkCard"
 
-const Work = () => {
-    return (
-        <div>
-            <h1 className="content-text">My Projects</h1>
-            <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        </div>
-    )
+class Work extends Component {
+    constructor() {
+        super();
+        this.state = {
+            works: []
+        };
+    }
+    componentDidMount() {
+        axios.get('./src/data/works.json')
+            .then(response => {
+                this.setState({
+                    works: response.data
+                })
+            })
+    }
 
+    render() {
+        const works = this.state.works
+        let worksList 
+
+        if (works.length > 0) {
+            worksList = works.map(work => {
+                return (
+                    <div key={work.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                        <WorkCard work={work} />
+                    </div>
+                )
+            })
+        }
+
+
+        return (
+            <div className="content-wrapper">
+                <div className="flex flex-wrap -mx-2 lg:-mx-1">
+                    {worksList}
+                </div>
+            </div>
+        )
+    
+    }
 };
 
 export default Work
