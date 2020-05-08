6fd2daa8ac8cf5cef07421308153a4b14e54d419
Dash <daria.lykova@gmail.com>
2020-05-08 13:08:59 +0200

    Troubleshooting deployment fails

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
         loader: "babel-loader",
         options: { presets: ["@babel/env"] }
       },
@@ -21,7 +20,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "./dist/",
     filename: "bundle.js"
   },
   devServer: {
