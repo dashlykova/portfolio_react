47b0f2170858217d6c1b83f52480f27c2c5c3ba7
Dash <daria.lykova@gmail.com>
2020-05-08 14:36:55 +0200

    more package shenannigans

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: { presets: ["@babel/preset-env"] }
       },
       {
         test: /\.css$/,
