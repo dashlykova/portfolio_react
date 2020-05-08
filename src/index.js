a1cdbb4537a4392424059cf762c57fc56bad0194
Dash <daria.lykova@gmail.com>
2020-05-08 17:08:28 +0200

    case sensitivity?

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ const App = () => {
                     <Switch>
                         <BrowserRouter basename="/portfolio_react" />
                         <Route exact path='/' component={Hello}></Route>
-                        <Route exact path='/about' component={About}></Route>
-                        <Route exact path='/projects' component={Projects}></Route>
-                        <Route exact path='/work' component={Work}></Route>
+                        <Route exact path='/About' component={About}></Route>
+                        <Route exact path='/Projects' component={Projects}></Route>
+                        <Route exact path='/Work' component={Work}></Route>
                     </Switch>
                 </div>
             </div >
